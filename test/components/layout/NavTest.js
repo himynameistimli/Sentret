ba9de4b4cacd3e1108d9194e2aae275b088a0e89
Timothy Li <himynameistimli@gmail.com>
2016-07-09 16:28:24 +0800

    Add react-router & add function test

--- test/components/layout/NavTest.js
+++ test/components/layout/NavTest.js
@@ -11,21 +11,17 @@ import createComponent from 'helpers/shallowRenderHelper';
 import Nav from 'components/layout/Nav.js';
 
 describe('Nav', () => {
-    let component, button;
-
+    let component;
+    let button;
     beforeEach(() => {
         component = createComponent(Nav);
-
-        // button = TestUtils.renderIntoDocument(
-        //     <Nav />
-        // );
+        button = TestUtils.renderIntoDocument(<Nav />);
     });
 
-    // it('should toggle collapse menu when set', () => {
-    //     let navButton = TestUtils.findRenderedDOMComponentWithClass(button, 'navbar-toggle');
-    //     TestUtils.Simulate.click(navButton)
-    //     expect(component.state.collapsed).to.equal(true);
-    // });
+    it('should toggle collapse menu when set', () => {
+        button.toggleCollapse();
+        expect(button.state.collapsed).to.equal(false);
+    });
 
     it('should have its component name as tagName', () => {
         expect(component.type).to.equal('nav');
